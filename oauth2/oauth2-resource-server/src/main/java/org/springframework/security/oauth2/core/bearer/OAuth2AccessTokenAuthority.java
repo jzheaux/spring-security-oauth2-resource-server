57dd997907ff4d2cc46c390e1405bcf731323cd2
Josh Cummings <josh.cummings@gmail.com>
2018-03-20 14:44:28 -0600

    Add Apache License

--- oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/core/bearer/OAuth2AccessTokenAuthority.java
+++ oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/core/bearer/OAuth2AccessTokenAuthority.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.security.oauth2.core.bearer;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -22,7 +37,7 @@ public class OAuth2AccessTokenAuthority implements GrantedAuthority {
 	 *
 	 */
 	public OAuth2AccessTokenAuthority(Map<String, Object> claims) {
-		this.authority = "ROLE_USER"; //TODO what should go here?? I suppose that this is, indeed a user, but really, I wouldn't encourage folks to use it to make decisions
+		this.authority = "ROLE_USER";
 		this.claims = claims;
 	}
 
--- samples/boot/oauth2/src/main/java/org/springframework/security/samples/oauth2/rs/auth0/MessagesApplication.java
+++ samples/boot/oauth2/src/main/java/org/springframework/security/samples/oauth2/rs/auth0/MessagesApplication.java
@@ -1,18 +1,24 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.security.samples.oauth2.rs.auth0;
-
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Stream;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.RSAKeyProvider;
-import org.springframework.security.oauth2.resource.authentication.JwtEncodedOAuth2AccessTokenAuthenticationProvider;
-import org.springframework.security.oauth2.resource.web.BearerTokenAuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -25,12 +31,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.bearer.OAuth2AccessTokenAuthority;
+import org.springframework.security.oauth2.jwt.AccessTokenJwtVerifier;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.resource.authentication.JwtEncodedOAuth2AccessTokenAuthenticationProvider;
+import org.springframework.security.oauth2.resource.web.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.oauth2.jwt.AccessTokenJwtVerifier;
-import org.springframework.security.oauth2.core.bearer.OAuth2AccessTokenAuthority;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class MessagesApplication {
