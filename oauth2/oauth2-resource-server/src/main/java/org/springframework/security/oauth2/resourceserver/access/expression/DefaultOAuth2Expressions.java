d1adda1b334f9508ae7c8844b6158257d3b10f81
Josh Cummings <josh.cummings@gmail.com>
2018-04-20 14:56:01 -0600

    Move Expression Support under resourceserver package

--- oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/resourceserver/access/expression/DefaultOAuth2Expressions.java
+++ oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/resourceserver/access/expression/DefaultOAuth2Expressions.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.security.oauth2.core.expression;
+package org.springframework.security.oauth2.resourceserver.access.expression;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.ClaimAccessor;
