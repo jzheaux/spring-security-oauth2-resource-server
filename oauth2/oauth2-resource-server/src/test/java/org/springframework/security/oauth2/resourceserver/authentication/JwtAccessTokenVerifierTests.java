778e0cc3be569564b491bb128948b3c147e97792
Josh Cummings <josh.cummings@gmail.com>
2018-04-20 16:14:56 -0600

    Fix Error Message Formatting
    The error messages in JwtAccessTokenVerifier inadvertently used the
    wrong placeholder, {} instead of %s.
    
    Fixes: gh-32

---
+++ oauth2/oauth2-resource-server/src/test/java/org/springframework/security/oauth2/resourceserver/authentication/JwtAccessTokenVerifierTests.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.oauth2.resourceserver.authentication;
+
+import org.assertj.core.util.Maps;
+import org.junit.Test;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * @author Josh Cummings
+ */
+public class JwtAccessTokenVerifierTests {
+	@Test
+	public void verifyWhenJwtIsExpiredThenErrorMessageIndicatesExpirationTime() {
+		Instant expiry = Instant.MIN.plusSeconds(1);
+
+		Jwt jwt = new Jwt(
+				"token",
+				Instant.MIN,
+				expiry,
+				Maps.newHashMap("alg", JwsAlgorithms.RS256),
+				Maps.newHashMap(JwtClaimNames.EXP, expiry));
+
+		JwtAccessTokenVerifier verifier = new JwtAccessTokenVerifier();
+
+		assertThatThrownBy(() -> verifier.verify(jwt.getClaims()))
+				.hasMessageContaining("Jwt expired at " + expiry);
+	}
+
+	@Test
+	public void verifyWhenJwtIsTooEarlyThenErrorMessageIndicatesNotBeforeTime() {
+		Instant oneHourFromNow = Instant.now().plusSeconds(3600);
+
+		Jwt jwt = new Jwt(
+				"token",
+				Instant.MIN,
+				oneHourFromNow,
+				Maps.newHashMap("alg", JwsAlgorithms.RS256),
+				Maps.newHashMap(JwtClaimNames.NBF, oneHourFromNow));
+
+		JwtAccessTokenVerifier verifier = new JwtAccessTokenVerifier();
+
+		assertThatThrownBy(() -> verifier.verify(jwt.getClaims()))
+				.hasMessageContaining("Jwt used before " + oneHourFromNow);
+	}
+}
